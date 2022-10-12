3f9465bd0d60cec8fe486c72c6c7ca39b02a51c4
John McAlester <john@thriveglobal.com>
2022-10-12 15:37:33 -0700

    Add song atom and play state

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { 
-    HomeIcon,
-    PlusCircleIcon,
-} from '@heroicons/react/24/outline';
+import HomeIcon from '@heroicons/react/24/outline/HomeIcon';
+import MagnifyingGlassIcon from '@heroicons/react/24/outline/MagnifyingGlassIcon';
+import BuildingLibraryIcon from '@heroicons/react/24/outline/BuildingLibraryIcon';
+import PlusCircleIcon from '@heroicons/react/24/outline/PlusCircleIcon';
+import HeartIcon from '@heroicons/react/24/outline/HeartIcon';
+import RssIcon from '@heroicons/react/24/outline/RssIcon';
 import { signOut, useSession } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { playlistIdState } from '../atoms/playlistAtom';
@@ -14,8 +16,6 @@ function Sidebar() {
   const [playlists, setPlaylists] = useState([]);
   const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
 
-  console.log("You picked playlist >>>", playlistId);
-
   useEffect(() => {
     if(spotifyApi.getAccessToken()) {
       spotifyApi.getUserPlaylists().then((data) => {
@@ -24,43 +24,39 @@ function Sidebar() {
     }
   }, [session, spotifyApi]);
 
-  console.log(playlists);
-
   return (
     <div className="text-gray-500 p-5 text-xs lg:text-sm border-r-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
-        <button 
-          className="flex items-center space-x-2 hover:text-white" 
-          onClick={() => signOut()}>
-            <p>logout</p>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="w-5 h-5" /> 
+          <p>Home</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-            <HomeIcon className="w-5 h-5" />
-            <p>Home</p>
-        </button>
-        {/* <button className="flex items-center space-x-2 hover:text-white">
-            <SearchIcon className="w-5 h-5" />
-            <p>Search</p>
+          <MagnifyingGlassIcon className="w-5 h-5" />
+          <p>Search</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-            <LibraryIcon className="w-5 h-5"  />
-            <p>Your Library</p>
-        </button> */}
+          <BuildingLibraryIcon className="w-5 h-5" />
+          <p>Library</p>
+        </button>
+
         <hr className="border-t-[0.1px] border-gray-900"></hr>
 
         <button className="flex items-center space-x-2 hover:text-white">
-            <PlusCircleIcon className="w-5 h-5" />
-            <p>Create Playlist</p>
-        </button>
-        {/* <button className="flex items-center space-x-2 hover:text-white">
-            <HeartIcon className="w-5 h-5" />
-            <p>Liked Songs</p>
+          <PlusCircleIcon className="w-5 h-5" /> 
+          <p>Add Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-            <RssIcon className="w-5 h-5"  />
-            <p>Your Episodes</p>
-        </button> */}
+          <HeartIcon className="w-5 h-5" />
+          <p>Like</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="w-5 h-5" />
+          <p>Episodes</p>
+        </button>
+
         <hr className="border-t-[0.1px] border-gray-900"></hr>
+
         {playlists.map((playlist) => 
           <p 
           key={playlist.id} 
@@ -69,7 +65,7 @@ function Sidebar() {
           >
             {playlist.name}
           </p>
-        )}
+        )};
       </div>
     </div>
   );
